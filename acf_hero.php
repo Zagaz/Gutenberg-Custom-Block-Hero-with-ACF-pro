c0a70af0dfa91a07a629384f00a83a0e6e95f3f1
Zagaz <andre_ranulfo@hotmail.com>
2025-08-12 13:38:28 -0300

    Implement ACF Hero block with associated styles and scripts

---
+++ acf_hero.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: ACF Block
+ * Description: A simple ACF block example.
+ * Version: 1.0.0
+ * Author: Andre Ranulfo
+ */
+
+// You shall not pass! 
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+
+
+add_action('acf/init', 'acf_hero_init');
+
+function acf_hero_init() {
+    $block_name = 'acf-hero'; // Block name (should be lowercase and without spaces)
+    $block_title = 'ACF Hero';
+    $block_text_domain = 'acf-block';
+    $block_render_template = 'blocks/testimonial/testimonial.php';
+
+    if ( function_exists( 'acf_register_block_type' ) ) {
+        acf_register_block_type( array(
+            'name'              => $block_name,
+            'title'             => __( $block_title , $block_text_domain ),
+            'description'       => __( 'A custom hero block.', $block_text_domain ),
+            'render_template'   => plugin_dir_path( __FILE__ ) . $block_render_template,
+            'category'          => 'formatting',
+            'icon'              => 'superhero-alt',
+            'keywords'          => array( 'hero', 'quote' ),
+            'enqueue_style'     => plugin_dir_url( __FILE__ ) . 'assets/css/style.css',
+            'enqueue_script'    => plugin_dir_url( __FILE__ ) . 'assets/js/script.js',
+        ) );
+    }
+}
+
+
+
+
+
